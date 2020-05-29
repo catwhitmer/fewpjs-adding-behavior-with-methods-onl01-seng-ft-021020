d48dfe3568b1f93b2bf3eb7bee47d3ce9d3d7231
Cathrin Whitmer <catwhitmer@gmail.com>
2020-05-29 16:21:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,8 +26,11 @@ class Bird {
     this.sex = sex;
   }
   
-  speak() {
- 
+ speak() {
+    if (this.sex === "male") {
+      return `It's me! ${this.name}, the parrot!`
+    } else {
+      return `${this.name} says squawk!`
+    }
   }
 }
-
